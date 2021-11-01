0625009b54d1b880c793c777229a83c88584f10a
Kerim Balic <balickerim@gmail.com>
2021-11-01 16:52:17 +0100

    added cors in app

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = require('./router/router');
 const app = express();
 const port = process.env.PORT || 9000;
+const cors = require('cors');
 
+app.use(cors());
 app.use(router);
 
 app.listen(port, () => {
